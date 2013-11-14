92c7c819f3322835d51b98d978edaecb101bf5f4
unknown <Seung@Seung-PC.(none)>
2013-11-14 06:05:18 -0800

    Done did the plan columns UI

--- health/health/static/js/home.js
+++ health/health/static/js/home.js
@@ -3,4 +3,6 @@ $(document).ready(function(){
         no_results_text: "Sorry, we don't support that state yet."
     });
 
+    var highestCol = Math.max($('.info-col').height(),$('.data-col').height());
+    $('.standard-col').height(highestCol);
 })
