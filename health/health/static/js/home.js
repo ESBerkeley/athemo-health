538efdbfe4fe8431215dafe4f9e5ab29fe308870
Tony Hung <hungyukao92@gmail.com>
2013-11-17 16:35:11 -0800

    Add person/delete person row

--- health/health/static/js/home.js
+++ health/health/static/js/home.js
@@ -5,4 +5,33 @@ $(document).ready(function(){
 
     var highestCol = Math.max($('.info-col').height(),$('.data-col').height());
     $('.standard-col').height(highestCol);
+
+    var peopleCount = 1;
+    
+    $("#add_person").click(function() {
+    	$(".delete_person").css("display", "none");
+    	peopleCount++;
+    	$("#person_rows").append(
+			"<div id='person_" + peopleCount + "' class='row person-row'> \
+                <div class='col-md-4 person-label'>Person " + peopleCount + "</div> \
+                <div class='col-md-2 age-label'>Age</div> \
+                <div class='col-md-5'> \
+                    <div> \
+                        <input type='text' class='form-control input-sm'/> \
+                    </div> \
+                </div> \
+                <div class='delete_person' personId=" + peopleCount + " > \
+                    <div class='glyphicon glyphicon-remove'></div> \
+                </div> \
+            </div>"
+		);
+    })
+
+    $("#person_rows").on("click", ".delete_person", function() {
+    	peopleCount--;
+    	var personId = $(this).attr("personId");
+    	$("#person_" + personId).remove();
+    	$("#person_" + peopleCount + " .delete_person").css("display", "inherit");
+    })
+
 })
