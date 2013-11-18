683193bd6f011f2026b77f099a2db915014c2409
Tony Hung <hungyukao92@gmail.com>
2013-11-17 17:12:51 -0800

    CSS for delete homes

--- health/health/static/js/home.js
+++ health/health/static/js/home.js
@@ -6,38 +6,10 @@ $(document).ready(function(){
 //    var highestCol = Math.max($('.info-col').height(),$('.data-col').height());
 //    $('.standard-col').height(highestCol);
 
-    var peopleCount = 1;
-    
-    $("#add_person").click(function() {
-    	$(".delete_person").css("display", "none");
-    	peopleCount++;
-    	$("#person_rows").append(
-			"<div id='person_" + peopleCount + "' class='row person-row'> \
-                <div class='col-md-4 person-label'>Person " + peopleCount + "</div> \
-                <div class='col-md-2 age-label'>Age</div> \
-                <div class='col-md-5'> \
-                    <div> \
-                        <input type='text' class='form-control input-sm'/> \
-                    </div> \
-                </div> \
-                <div class='delete_person' personId=" + peopleCount + " > \
-                    <div class='glyphicon glyphicon-remove'></div> \
-                </div> \
-            </div>"
-		);
-    })
-
-    $("#person_rows").on("click", ".delete_person", function() {
-    	peopleCount--;
-    	var personId = $(this).attr("personId");
-    	$("#person_" + personId).remove();
-    	$("#person_" + peopleCount + " .delete_person").css("display", "inherit");
-    })
-
     generate_donuts();
+    adjust_person_rows();
 
 })
-
 
 function generate_donuts() {
     var dataset = {
@@ -81,4 +53,35 @@ function generate_donuts() {
         .attr("fill", function(d, i) { return color(i); })
         .attr("d", arc);
 
+}
+
+function adjust_person_rows() {
+
+	var peopleCount = 1;
+    
+    $("#add_person").click(function() {
+    	$(".delete_person").css("display", "none");
+    	peopleCount++;
+    	$("#person_rows").append(
+			"<div id='person_" + peopleCount + "' class='row person-row'> \
+                <div class='col-md-4 person-label'>Person " + peopleCount + "</div> \
+                <div class='col-md-2 age-label'>Age</div> \
+                <div class='col-md-5'> \
+                    <div> \
+                        <input type='text' class='form-control input-sm'/> \
+                    </div> \
+                </div> \
+                <div class='delete_person' personId=" + peopleCount + " > \
+                    <div class='glyphicon glyphicon-remove'></div> \
+                </div> \
+            </div>"
+		);
+    })
+
+    $("#person_rows").on("click", ".delete_person", function() {
+    	peopleCount--;
+    	var personId = $(this).attr("personId");
+    	$("#person_" + personId).remove();
+    	$("#person_" + peopleCount + " .delete_person").css("display", "inherit");
+    })
 }
