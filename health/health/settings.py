e9b1b8d492e2a40b620c3ad94a701fb243f42aa7
Eric Xiao <exiao@buynear.me>
2013-11-17 18:28:35 -0800

    new popover placements + href

--- health/health/settings.py
+++ health/health/settings.py
@@ -135,6 +135,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'static_precompiler',
+    'django_evolution',
 )
 
 # A sample logging configuration. The only tangible logging
