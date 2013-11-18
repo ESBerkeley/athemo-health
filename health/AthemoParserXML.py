803b2e5a863733036c1616e7f508efe57750ac56
Hemant Bhonsle <hbhonsle1@berkeley.edu>
2013-11-17 16:19:51 -0800

    small fix and variables global

--- health/AthemoParserXML.py
+++ health/AthemoParserXML.py
@@ -12,44 +12,36 @@ from openpyxl import load_workbook
 import sys
 import glob
 
+book = xlrd.open_workbook("Marketplace_premium_databook_2014.xlsx")
+sheet = book.sheet_by_name("Zip Code-Rating Area Lookup")
+
 def get_zip_code_info(zip):
-    book = xlrd.open_workbook("Marketplace_premium_databook_2014.xlsx")
-    sheet = book.sheet_by_name("Zip Code-Rating Area Lookup")
     for row_num in xrange(sheet.nrows):
         if sheet.row(row_num)[3].value == str(zip):
             return sheet.row(row_num)
         
 def get_state_name(zip):
-    book = xlrd.open_workbook("Marketplace_premium_databook_2014.xlsx")
-    sheet = book.sheet_by_name("Zip Code-Rating Area Lookup")
     for row_num in xrange(sheet.nrows):
         if sheet.row(row_num)[3].value == str(zip):
             return sheet.row(row_num)[0].value
         
 def get_county_code(zip):
-    book = xlrd.open_workbook("Marketplace_premium_databook_2014.xlsx")
-    sheet = book.sheet_by_name("Zip Code-Rating Area Lookup")
     for row_num in xrange(sheet.nrows):
         if sheet.row(row_num)[3].value == str(zip):
             return sheet.row(row_num)[1].value
         
 def get_county_name(zip):
-    book = xlrd.open_workbook("Marketplace_premium_databook_2014.xlsx")
-    sheet = book.sheet_by_name("Zip Code-Rating Area Lookup")
     for row_num in xrange(sheet.nrows):
         if sheet.row(row_num)[3].value == str(zip):
             return sheet.row(row_num)[2].value
         
 def get_rating_area(zip):
-    book = xlrd.open_workbook("Marketplace_premium_databook_2014.xlsx")
-    sheet = book.sheet_by_name("Zip Code-Rating Area Lookup")
     for row_num in xrange(sheet.nrows):
         if sheet.row(row_num)[3].value == str(zip):
             return sheet.row(row_num)[4].value
 
 # stateInitials must be entered in '', and rating should be entered as x.0, so 1 is 1.0
 def get_state_rating_info(stateInitials, rating):
-    book = xlrd.open_workbook("Marketplace_premium_databook_2014.xlsx")
     sheet = book.sheet_by_name(stateInitials)
     for row_num in xrange(sheet.nrows):
         print(sheet.row(row_num)[3].value)
@@ -60,7 +52,7 @@ def safs(stateInitials, age):
     if age > 0 && age < 21:
         wb=load_workbook('0to20.xlsx')
     elif age > 20 && age < 23:
-        wb=load_workbook('25.xlsx')
+        wb=load_workbook('21.xlsx')
     elif age > 22 && age < 28:
         wb=load_workbook('25.xlsx')
     elif age > 27 && age < 33:
