29fe07268d02e14018ca43a3aa7f40f5d4569ffc
Hemant Bhonsle <hbhonsle1@berkeley.edu>
2013-11-17 20:09:23 -0800

    added a bunch of parsers for diff health plans

--- health/AthemoParserXML.py
+++ health/AthemoParserXML.py
@@ -130,4 +130,112 @@ def getValletHealthPlan(tier, age):
     elif str(tier) == 'catastrophic':
         return sheetObama.cell('CS' + base2).value
 
+def getContraHealthPlan(tier, age):
+    sheetObama = bookObama.get_sheet_by_name("Contra Costa Health Plan")
+    base = 21
+    if age > 20:
+        base = age + base - 20
+    base2 = str(base)
+    if str(tier) == 'platinum':
+        return sheetObama.cell('BP' + base2).value
+    elif str(tier) == 'gold':
+        return sheetObama.cell('BQ' + base2).value
+    elif str(tier) == 'silver':
+        return sheetObama.cell('BR' + base2).value
+    elif str(tier) == 'bronze':
+        return sheetObama.cell('BN' + base2).value
+    elif str(tier) == 'catastrophic':
+        return sheetObama.cell('BO' + base2).value
+
+def getSharpHealthPlan(tier, age):
+    sheetObama = bookObama.get_sheet_by_name("Sharp Health Plan")
+    base = 21
+    if age > 20:
+        base = age + base - 20
+    base2 = str(base)
+    if str(tier) == 'platinum':
+        return sheetObama.cell('JM' + base2).value
+    elif str(tier) == 'gold':
+        return sheetObama.cell('JN' + base2).value
+    elif str(tier) == 'silver':
+        return sheetObama.cell('JO' + base2).value
+    elif str(tier) == 'bronze':
+        return sheetObama.cell('JP' + base2).value
+    elif str(tier) == 'catastrophic':
+        return sheetObama.cell('JQ' + base2).value
+
+#only has platinum gold and silver
+def getCaNetHealthPlan(tier, age):
+    sheetObama = bookObama.get_sheet_by_name("Health Net of California")
+    base = 12
+    if age > 20:
+        base = age + base - 20
+    base2 = str(base)
+    if str(tier) == 'platinum':
+        return sheetObama.cell('HY' + base2).value
+    elif str(tier) == 'gold':
+        return sheetObama.cell('HZ' + base2).value
+    elif str(tier) == 'silver':
+        return sheetObama.cell('IA' + base2).value
+
+#can only take in rating areas 4 and 8
+def getChineseHealthPlan(tier, age, area):
+    sheetObama = bookObama.get_sheet_by_name("Chinese Community Health Plan")
+    base = 0
+    if area != 4 and area != 8:
+        return 'area not covered'
+    elif area == 4:
+        base = 13
+    elif area == 8:
+        base = 63
+    
+    if age > 20:
+        base = base + age - 20
+    base2 = str(base)
+
+    if str(tier) == 'platinum':
+        return sheetObama.cell('B' + base2).value
+    elif str(tier) == 'gold':
+        return sheetObama.cell('E' + base2).value
+    elif str(tier) == 'silver':
+        return sheetObama.cell('H' + base2).value
+    elif str(tier) == 'bronze':
+        return sheetObama.cell('K' + base2).value
+    elif str(tier) == 'catastrophic':
+        return sheetObama.cell('N' + base2).value
+
+#can only take in rating areas 15,16,17, and 19
+def getMolinaHealthPlan(tier, age, area):
+    sheetObama = bookObama.get_sheet_by_name("Molina Healthcare of California")
+    base = 0
+    if area != 15 and area != 16 and area != 17 and area != 19:
+        return 'area not covered'
+    elif area == 15:
+        base = 8
+    elif area == 16:
+        base = 53
+    elif area == 17:
+        base = 98
+    elif area == 19:
+        base = 143
+
+    if age > 20:
+        base = base + age - 20
+    base2 = str(base)
+    
+    if str(tier) == 'platinum':
+        return sheetObama.cell('A' + base2).value
+    elif str(tier) == 'gold':
+        return sheetObama.cell('B' + base2).value
+    elif str(tier) == 'silver':
+        return sheetObama.cell('C' + base2).value
+    elif str(tier) == 'bronze':
+        return sheetObama.cell('D' + base2).value
+    elif str(tier) == 'catastrophic':
+        return sheetObama.cell('E' + base2).value
+
+
+
+
+
 
