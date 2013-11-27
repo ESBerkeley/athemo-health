ac83fc72d9dc4fe6db4d38ab3f3ad5c3096340c1
Eric Xiao <exiao@buynear.me>
2013-11-26 16:12:07 -0800

    reformat

--- health/insuranceapp/utils.py
+++ health/insuranceapp/utils.py
@@ -2,6 +2,7 @@ __author__ = 'ericxiao'
 from models import GeographicArea, HealthcarePlan, Provider
 from AthemoParserXML import *
 
+
 def get_plans_data(ages, zip_code, income, prescription_use, doctor_use):
     '''
     Your premium + your deductible + any coinsurance
@@ -147,6 +148,7 @@ PLATINUM = {'primary_care_copay': 20,
           'avg_prescription_copay': .78*19 + .22*50,
           'max_out_of_pocket': 4000,
           'max_out_of_pocket_family': 8000}
+
 MEDAL_DETAILS = {'Bronze': BRONZE, 'Silver': SILVER, 'Gold': GOLD, 'Platinum': PLATINUM, 'Catastrophic': BRONZE}
 
 AGE_RATIOS = {20: 0.635, 21: 1, 22: 1, 23: 1, 24: 1, 25: 1.004, 26: 1.024, 27: 1.048, 28: 1.087, 29: 1.119, 30: 1.135, 31: 1.159, 32: 1.183, 33: 1.198, 34: 1.214, 35: 1.222, 36: 1.23, 37: 1.238, 38: 1.246, 39: 1.262, 40: 1.278, 41: 1.302, 42: 1.325, 43: 1.357, 44: 1.397, 45: 1.444, 46: 1.5, 47: 1.563, 48: 1.635, 49: 1.706, 50: 1.786, 51: 1.865, 52: 1.952, 53: 2.04, 54: 2.135, 55: 2.23, 56: 2.333, 57: 2.437, 58: 2.548, 59: 2.603, 60: 2.714, 61: 2.81, 62: 2.873, 63: 2.952, 64: 3}
